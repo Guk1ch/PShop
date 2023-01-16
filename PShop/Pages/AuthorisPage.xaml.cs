d21803295a3c9858796210f2004b7c2f94713fd3
201903 <201903@TOCHKA-008>
2023-01-16 19:51:44 +0300

    Добавьте файлы проекта.

---
+++ PShop/Pages/AuthorisPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using PShop.DataBase;
+
+namespace PShop.Pages
+{
+	/// <summary>
+	/// Логика взаимодействия для AuthorisPage.xaml
+	/// </summary>
+	public partial class AuthorisPage : Page
+	{
+		public static User user { get; set; }
+		public AuthorisPage()
+		{
+			InitializeComponent();
+		}
+		private void btnReg_Click(object sender, RoutedEventArgs e)
+		{
+			NavigationService.Navigate(new RegPage());
+		}
+
+		private void btnEnter_Click(object sender, RoutedEventArgs e)
+		{
+			string login = tbLog.Text.Trim();
+			string password = tbPass.Password.Trim();
+			user = BdConnection.connection.User.Where(x => x.Login == login && x.Password == password).FirstOrDefault();
+			if(user == null)
+			{
+				MessageBox.Show("Все не гуд");
+			}
+			else if (user != null)
+			{
+				MessageBox.Show("Все гуд");
+			}
+
+		}
+	}
+}
---
+++ PShop/Pages/RegPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using PShop.DataBase;
+
+namespace PShop.Pages
+{
+	/// <summary>
+	/// Логика взаимодействия для RegPage.xaml
+	/// </summary>
+	public partial class RegPage : Page
+	{
+		
+		public RegPage()
+		{
+			InitializeComponent();
+		}
+		private void btnBackClick(object sender, RoutedEventArgs e)
+		{
+			NavigationService.Navigate(new AuthorisPage());
+		}
+
+		private void btnRegClick(object sender, RoutedEventArgs e)
+		{
+			User user = new User();
+			string Log = tbLog.Text.Trim();
+			string Pass = tbPass.Text.Trim();
+			user = BdConnection.connection.User.Where(x => x.Login == Log).FirstOrDefault();
+			if(user == null)
+			{
+				User newUser = new User();
+				newUser.Login = Log;
+				newUser.Password = Pass;
+				BdConnection.connection.User.Add(newUser);
+				BdConnection.connection.SaveChanges();
+				NavigationService.Navigate(new AuthorisPage());
+			}
+			else
+			{
+				MessageBox.Show("Что то не так");
+			}
+
+		}
+	}
+}
